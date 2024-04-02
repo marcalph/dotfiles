a708a2eadfce983d2d8d36d74c0d5f7d4fedad16
marcalph <alphonsus.marc@gmail.com>
2024-04-03 00:06:40 +0200

    home config + aliases

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -12,8 +12,7 @@
     stateVersion = "23.11"; # Please read the comment before changing.
     # home.packages option allows install of nix packages user profile
     packages = with pkgs; [
-      tree
-      less
+      tldr
       (pkgs.nerdfonts.override { fonts = ["Hack"]; })
     ];
   # # Required to get the fonts installed by home-manager to be picked up by OS.
@@ -28,17 +27,25 @@
   fonts.fontconfig.enable = true;
   programs.bat.enable = true;
   programs.bat.config.theme = "TwoDark";
+  programs.eza.enable = true;
+  programs.eza.enableZshIntegration = true;
   programs.fzf.enable = true;
   programs.fzf.enableZshIntegration = true;
-  programs.eza.enable = true;
   programs.git.enable = true;
   programs.zsh.enable = true;
   programs.zsh.enableCompletion = true;
+  programs.zsh.autosuggestion.enable = true;
+  programs.zsh.envExtra = "";
+  programs.zsh.history.ignoreDups = true;
   # programs.zsh.autosuggestion.enable =  true;
   programs.zsh.syntaxHighlighting.enable = true;
   programs.zsh.shellAliases = {
-    ll = "ls -hailF --color=auto";
-    ls = "ls --color=auto -F";
+    ll = "eza -hailF --icons";
+    ls = "eza --grid --icons";
+    tree = "eza --tree --icons"; 
+    ga = "git add";
+    gc = "git commit";
+    gs = "git status | head -n 2; exa --git -l";
     nixswitch = "darwin-rebuild switch --flake flake.nix";
   };
   programs.alacritty = {
@@ -48,6 +55,6 @@
   };
   programs.starship.enable = true;
   programs.starship.enableZshIntegration = true;
-    # home.file.".inputrc".source = ./dotfiles/inputrc;
+  # home.file.".inputrc".source = ./dotfiles/inputrc;
 
 }
