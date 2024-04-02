d3fea1acfbbfce00acb484ac65027c150e15bc25
marcalph <alphonsus.marc@gmail.com>
2024-04-02 16:22:41 +0200

    update darwin config

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,36 +1,49 @@
-{ config, pkgs, ... }:
-
-{
+{pkgs, ... }: {
   environment.systemPackages =
     [
       pkgs.home-manager
     ];
 
-  # Auto upgrade nix package and the daemon service.
+  # Auto upgrade nix package and the daemon 
   services.nix-daemon.enable = true;
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
   nix = {
     package = pkgs.nix;
     settings = {
       "extra-experimental-features" = [ "nix-command" "flakes" ];
     };
   };
-
-  # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;
-  # Used for backwards compatibility, please read the changelog before changing.
-  # $ darwin-rebuild changelog
-  system.stateVersion = 4;
-
-  # Install fonts
-  # fonts.fontDir.enable = true;
-  # fonts.fonts = [
-  #   pkgs.monaspace
-  # ];
-
+  # set some OSX preferences 
+  system.defaults = {
+    # minimal dock
+    dock = {
+      autohide = true;
+      orientation = "bottom";
+      show-process-indicators = false;
+      show-recents = false;
+      static-only = true;
+    };
+    NSGlobalDomain = {
+      AppleShowAllExtensions = true;
+      InitialKeyRepeat = 14;
+      KeyRepeat = 1;
+    };
+    # a finder that tells me what I want to know and lets me work
+    finder = {
+      AppleShowAllExtensions = true;
+      ShowPathbar = true;
+      _FXShowPosixPathInTitle = true;
+    };
+  };
+  system.keyboard.enableKeyMapping = true;
+  system.keyboard.remapCapsLockToEscape = true;
   # Use homebrew to install casks and Mac App Store apps
   homebrew = {
     enable = true;
-
     # casks = [
     #   "1password"
     #   "bartender"
@@ -53,21 +66,4 @@
     # };
   };
 
-  # set some OSX preferences that I always end up hunting down and changing.
-  system.defaults = {
-    # minimal dock
-    dock = {
-      autohide = true;
-      orientation = "right";
-      show-process-indicators = false;
-      show-recents = false;
-      static-only = true;
-    };
-    # a finder that tells me what I want to know and lets me work
-    finder = {
-      AppleShowAllExtensions = true;
-      ShowPathbar = true;
-      FXEnableExtensionChangeWarning = false;
-    };
-  };
 }
