1660bbe79a41438b6c772579be17bda94440f92c
marcalph <alphonsus.marc@gmail.com>
2024-04-24 12:43:52 +0200

    housekeeping

--- flake.nix
+++ flake.nix
@@ -3,13 +3,17 @@
 
   inputs = {
     # pin nixpkgs and home-manager version
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-23.11-darwin";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-23.11-darwin";
     # manages config and links stuff into homedir
-    home-manager.url = "github:nix-community/home-manager/release-23.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     # control system level config for macOS
-    nix-darwin.url = "github:LnL7/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-darwin = {
+      url = "github:lnl7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ nixpkgs, home-manager, nix-darwin, ... }:{
