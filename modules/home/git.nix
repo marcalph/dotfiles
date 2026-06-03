7d1c936bcea47f1dd4a0bf899a29222de6c991d6
marcalph <marcalph@protonmail.com>
2026-06-03 18:05:12 +0200

    update git config

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -12,6 +12,9 @@
       user.name = "marcalph";
       user.email = "marcalph@protonmail.com";
       init.defaultBranch = "main";
+      rerere.enabled = true;
+      pull.rebase = true;
+      rebase.autoStash = true;
     };
   };
 }
